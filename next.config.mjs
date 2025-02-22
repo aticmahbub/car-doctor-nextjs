657d1808fba7b4a9fd2b4b8645c141603235e4f9
aticcmahbub <aticcmahbub@gmail.com>
2025-02-22 19:53:29 +0600

    feat(ui): add services section

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.ibb.co',
+                // port: '',
+                // pathname: '/account123/**',
+                // search: '',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
